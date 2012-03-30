92e284eaf81e91f5a84cd2b7a9b1a6b8683597fa
Graham Poulter <graham@mocality.com>
2012-03-30 10:37:21 +0200

    Update setup.sh to work from outside /home/graham

--- setup.sh
+++ setup.sh
@@ -5,32 +5,36 @@ RCONF="${ACONF##$HOME/}"
 
 # Configure main config files to reference these ones
 declare -A configs
-configs['.bashrc']="source ~/$RCONF/bashrc"
-configs['.screenrc']="source ~/$RCONF/screenrc"
-configs['.inputrc']="\$include ~/$RCONF/inputrc"
-configs['.hgrc']="%include ~/$RCONF/hgrc"
+configs['.bashrc']="source $ACONF/bashrc"
+configs['.screenrc']="source $RCONF/screenrc"
+configs['.inputrc']="\$include $RCONF/inputrc"
+configs['.hgrc']="%include $RCONF/hgrc"
+
+# Work from home directory
+cd "$HOME"
 
 # Insert the includes on line 1 of the file
 function install {
     for file in "${!configs[@]}"; do
         val=${configs["$file"]}
-        if ! [[ -s ~/$file ]]; then
-            echo "echo $val > ~/$file"
-            echo $val > ~/$file
-        elif ! grep -q "$val" ~/$file; then
+        if ! [[ -s $file ]]; then
+            echo "echo $val > $file"
+            echo $val > $file
+        elif ! grep -q "$val" $file; then
             if [[ "$file" != '.bashrc' ]]; then
-                echo "sed -i "1i $val" ~/$file"
-                sed -i "1i $val" ~/$file
+                echo "sed -i "1i $val" $file"
+                sed -i "1i $val" $file
             else
-                echo "echo $val >> ~/$file"
-                echo $val >> ~/$file
+                echo "echo $val >> $file"
+                echo $val >> $file
             fi
         fi
     done
     # Link files that lack inclusion
     for rc in nanorc pylintrc; do
-        if ! [[ -e ~/.$rc ]] || ! diff -u ~/.$rc $ACONF/$rc; then
-            ln -vs $RCONF/$rc ~/.$rc
+        if [[ ! -e .$rc ]] || ! diff -u .$rc $RCONF/$rc; then
+            rm -f .$rc
+            ln -vs $RCONF/$rc .$rc
         fi
     done
 }
@@ -39,14 +43,14 @@ function install {
 function uninstall {
     for file in "${!configs[@]}"; do
         val=${configs["$file"]}
-        if grep -q "$val" ~/$file; then
-            echo "sed -i "\@^$val@d" ~/$file"
-            sed -i "\@^$val@d" ~/$file
-            [[ -s ~/$file ]] || rm -v ~/$file
+        if grep -q "$val" $file; then
+            echo "sed -i "\@^$val@d" $file"
+            sed -i "\@^$val@d" $file
+            [[ -s $file ]] || rm -v $file
         fi
     done
     for rc in nanorc pylintrc; do
-        [[ -e ~/.$rc ]] && diff -q ~/.$rc $ACONF/$rc && rm -v ~/.$rc
+        [[ -e .$rc ]] && diff -q .$rc $RCONF/$rc && rm -v .$rc
     done
 }
 
